dafd2b4c95b72055b86c991b31361ad12eabc599
Dan <njdan.5691@gmail.com>
2019-02-01 08:08:27 -0500

    Update init.el

--- init.el
+++ init.el
@@ -481,8 +481,8 @@
   :ensure nil
   :config
   (defun zz:macro-query (arg)
-    "Prompt for input using minibuffer during kbd macro execution.                                 \                                                                                       
-With prefix argument, allows you to select what prompt string to use.                              \                                                                                       
+    "Prompt for input using minibuffer during kbd macro execution.
+With prefix argument, allows you to select what prompt string to use.
 If the input is non-empty, it is inserted at point."
     (interactive "P")
     (let* ((prompt (if arg (read-from-minibuffer "PROMPT: ") "Input: "))
