ab4a3ff07a5d6e52a06a6f2c26f2565197fa8e1b
Dan <njdan.5691@gmail.com>
2019-02-07 13:33:36 -0500

    Update macros.el

--- macros.el
+++ macros.el
@@ -1,16 +1,8 @@
-
-
 (fset 'clean-nfo
       (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("^[<^L^S<title^M^F^@^L^S</^M^B^B^W^L^S<plot>^M^@^L^S</^B^B^B^W^D^L^Stagline^M^F^@^L^S</^M^B^B^W^L^Sp\
-remie^M^F^F^F^F^@^L^S</^M^B^B^L^Sact^B^L^S<name^M^F^@^L^S</^M^B^B^W^[<^L^S<title^M^F^X^S" 0 "%d")) arg)))                                                                                                                                                                                     
+remie^M^F^F^F^F^@^L^S</^M^B^B^L^Sact^B^L^S<name^M^F^@^L^S</^M^B^B^W^[<^L^S<title^M^F^X^S" 0 "%d")) arg)))
 (global-set-key [f6] 'clean-nfo)
 
 (fset 'clone-nfo
       (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("^@^E^[w^PC^Y^B^B^B^Knfo^Mg" 0 "%d")) arg)))
 (global-set-key [f5] 'clone-nfo)
-
-
-
-
-
-
