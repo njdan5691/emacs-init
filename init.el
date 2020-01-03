7000f1683719d80a6e2cd98b0dc39499305100d9
Dan <njdan.5691@gmail.com>
2020-01-03 10:48:15 -0500

    Update init.el

--- init.el
+++ init.el
@@ -4,36 +4,3 @@
     (url-copy-file url file))
   (load-file file))
 
-
-(if (file-directory-p "/home/dan/inc")
-    (use-package idev
-      ;;:disabled
-      :defer t
-      :quelpa (idev :fetcher github :repo "njdan5691/idev")
-      :bind(
-            :map zz:my-prefix
-                 ("p" . idev:select-project)
-                 ("y" . idev:mr-command)
-                 ("f" . idev:fcreate)
-                 ("s" . idev:submit)
-                 ("w" . idev:choose-mr)
-                 ("i" . idev:inc-grep)
-                 ("O" . idev:show-off)
-                 ("z" . idev:inc-files)
-                 ("Z" . idev:base-files)
-                 ("g" . idev:sget)
-                 ("G" . idev:sget-project-files)
-                 ("C" . idev:change-generic)
-                 :map dired-mode-map
-                 ("C-c d" . idev:dired-sdiff)
-                 ("C-c p" . idev:dired-edput)
-                 ("C-c s" . idev:dired-sget)
-                 ("C-l c" . compile)
-                 ("C-c e" . idev:dired-edget)
-                 :map zz:my-prefix
-                 :prefix-map idev-project-prefix
-                 :prefix "P"
-                 ("m" . idev:make-project)
-                 ("u" . idev:unfreeze-project)
-                 ("f" . idev:freeze-project)
-                 ("e" . idev:file-history))))
